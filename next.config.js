d600694c6d5cb70995babddb111be7fafe73d37a
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2024-10-29 21:22:01 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,12 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {}
 
-// module.exports = nextConfig
-// module.exports = {
-//     images: {
-//       domains: ['assets.shop.loblaws.ca'],
-//     },
-//   };
-
-
-// next.config.js
+module.exports = nextConfig
 module.exports = {
-  images: {
-    domains: ['assets.shop.loblaws.ca'],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false, // отключаем модули Node.js, если они не нужны
-      path: false,
-    };
-    return config;
-  },
-};
+    images: {
+      domains: ['assets.shop.loblaws.ca'],
+    },
+  };
+
+
 
