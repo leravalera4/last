facecc96ec8d14561f81830da2c982e3012ad10a
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2024-10-30 15:37:00 -0400

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import localFont from "next/font/local";
 import chart from "../../app/images/chart.svg";
@@ -6,8 +8,10 @@ import savings from "../../app/images/savings.svg";
 import grocery from "../../app/images/grocery.svg";
 import coins from "../../app/images/coins.svg";
 import sale from "../../app/images/sale.svg";
+import compare from "../../app/images/compare.svg";
+import store from "../../app/images/store.svg";
+import online from "../../app/images/online.svg";
 import cart from "../../app/images/cart.svg";
-
 
 const noir = localFont({
   src: [
@@ -36,79 +40,103 @@ const noir = localFont({
 
 const index = () => {
   return (
-    <div style={{paddingTop:"32px"}}>
-      <h1 style={{ fontSize: "20px" }} className={noir.className}>
+    <div style={{ marginLeft: "10%", marginRight: "10%" }}>
+      {" "}
+      <p
+        className={noir.className}
+        //className="lead"
+        style={{
+          boxSizing: "border-box",
+          marginTop: "3%",
+          marginBottom: "3rem",
+          fontSize: "1.25rem",
+          fontWeight: 700,
+          textAlign:"center"
+        }}
+      >
         Welcome to Shoppy Scan, your ultimate companion for savvy shopping and
         significant savings!
-      </h1>
-      <div
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-      >
+      </p>
+      <div style={{ display: "flex", alignItems: "center" }}>
         <Image src={chart} width={40} height={40} alt="chart" />
-        <h1
-          style={{
-            fontSize: "22px",
-            fontWeight: "600",
-            paddingBottom: "0px",
-            marginBottom: "0px",
-          }}
+        <h4
           className={noir.className}
+          style={{
+            boxSizing: "border-box",
+            marginTop: "0px",
+            marginBottom: "0.5rem",
+            lineHeight: 1.2,
+            fontSize: "1.5rem",
+            fontWeight: 700,
+          }}
         >
           Compare Prices in Different Stores
-        </h1>
+        </h4>
       </div>
       <p
-        style={{ marginTop: "2px", fontSize: "16px" }}
         className={noir.className}
+        style={{
+          boxSizing: "border-box",
+          marginTop: "0px",
+          marginBottom: "1rem",
+        }}
       >
         Easily compare the prices of your favorite products across various
         stores to ensure you're getting the best deals. Shoppy Scan empowers you
         to make informed decisions, helping you save on every item on your
         shopping list.
       </p>
-      <div
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-      >
+      <div style={{ display: "flex", alignItems: "center" }}>
         <Image src={sale} width={40} height={40} alt="chart" />
-        <h1
-          style={{
-            fontSize: "22px",
-            fontWeight: "600",
-            paddingBottom: "0px",
-            marginBottom: "0px",
-          }}
+        <h4
           className={noir.className}
+          style={{
+            boxSizing: "border-box",
+            marginTop: "0px",
+            marginBottom: "0.5rem",
+            lineHeight: 1.2,
+            fontSize: "1.5rem",
+            fontWeight: 700,
+          }}
         >
           Check What's on Sale
-        </h1>
+        </h4>
       </div>
       <p
-        style={{ marginTop: "2px", fontSize: "16px" }}
         className={noir.className}
+        style={{
+          boxSizing: "border-box",
+          marginTop: "0px",
+          marginBottom: "1rem",
+        }}
       >
         Stay in the loop with the latest discounts and promotions. Shoppy Scan
         allows you to quickly identify products currently on sale, making it a
         breeze to capitalize on special offers and maximize your savings.
       </p>
-      <div
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-      >
+      <div style={{ display: "flex", alignItems: "center" }}>
         <Image src={cart} width={40} height={40} alt="chart" />
-        <h1
-          style={{
-            fontSize: "22px",
-            fontWeight: "600",
-            paddingBottom: "0px",
-            marginBottom: "0px",
-          }}
+        <h4
           className={noir.className}
+          style={{
+            boxSizing: "border-box",
+            marginTop: "0px",
+            marginBottom: "0.5rem",
+            lineHeight: 1.2,
+            fontSize: "1.5rem",
+            fontWeight: 700,
+          }}
         >
           Add to Cart for Easy Tracking and Comparison
-        </h1>
+        </h4>
       </div>
       <p
-        style={{ marginTop: "2px", fontSize: "16px" }}
         className={noir.className}
+        style={{
+          boxSizing: "border-box",
+          marginTop: "0px",
+          marginBottom: "1rem",
+        }}
       >
         Simplify your shopping strategy by adding items to your virtual cart.
         Keep track of products you're interested in purchasing and receive
@@ -116,33 +144,6 @@ const index = () => {
         in your cart across different stores, ensuring you never miss out on a
         great deal.
       </p>
-      {/* <div
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-      >
-        <Image src={savings} width={40} height={40} alt="chart" />
-        <h1
-          style={{
-            fontSize: "22px",
-            fontWeight: "600",
-            paddingBottom: "0px",
-            marginBottom: "0px",
-          }}
-          className={noir.className}
-        >
-          Find Where It's Cheaper to Buy Today
-        </h1>
-      </div>
-      <p
-        style={{ marginTop: "2px", fontSize: "17px" }}
-        className={noir.className}
-      >
-     Take the guesswork out of your
-        shopping decisions. Shoppy Scan lets you discover the most
-        cost-effective options by comparing prices at different stores. Whether
-        it's your regular grocery run or a special purchase, make sure you're
-        getting the best value for your money. Save time, save money, and shop
-        smarter with Shoppy Scan!
-      </p> */}
     </div>
   );
 };
