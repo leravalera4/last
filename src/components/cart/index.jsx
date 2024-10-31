555d37df58bfaaab04f6128600d41c9518d743f9
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2024-10-31 11:21:09 -0400

    Update index.jsx

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -66,27 +66,17 @@ const Cart = () => {
 
   const pdfRef = useRef();
 
-  if (typeof localStorage !== 'undefined') {
-    localStorage.setItem('key', 'value');
-  } else if (typeof sessionStorage !== 'undefined') {
-    // Fallback to sessionStorage if localStorage is not supported
-    sessionStorage.setItem('key', 'value');
-  } else {
-    // If neither localStorage nor sessionStorage is supported
-    console.log('Web Storage is not supported in this environment CART.');
-  }
-
-
+  
   useEffect(() => {
     window.addEventListener("storage", ()=>{
         const stores = localStorage.getItem("stores");
         const storesArray = JSON.parse(stores);
         console.log(storesArray);
     })
-    if (typeof window !== 'undefined') {
-      window.addEventListener('storage', handleStorage);
-    }
-    return () => window.removeEventListener('storage', handleStorage);
+    // if (typeof window !== 'undefined') {
+    //   window.addEventListener('storage', handleStorage);
+    // }
+    // return () => window.removeEventListener('storage', handleStorage);
   }, []);
 
   React.useEffect(() => {
