bcf37cdd7579b5ea12144852ae56cc9e7602f4b8
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2024-10-31 12:19:03 -0400

    Update index.jsx

--- src/components/ab/index.jsx
+++ src/components/ab/index.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import localFont from "next/font/local";
-import chart from "../../app/images/chart.svg";
 import Image from "next/image";
-import savings from "../../app/images/savings.svg";
-import grocery from "../../app/images/grocery.svg";
-import coins from "../../app/images/coins.svg";
-import sale from "../../app/images/sale.svg";
 import compare from "../../app/images/compare.svg";
 import store from "../../app/images/store.svg";
 import online from "../../app/images/online.svg";
-import cart from "../../app/images/cart.svg";
+
 
 
 const noir = localFont({
@@ -39,7 +34,7 @@ const noir = localFont({
 
 const index = () => {
   return (
-    <div style={{paddingTop:"32px"}}>
+    <div style={{paddingTop:"32px",paddingBottom: "270px"}}>
       <div
         style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
       >
@@ -97,7 +92,7 @@ const index = () => {
           }}
           className={noir.className}
         >
-          Add to Cart for Total Price
+          Add to List for Total Price
         </h1>
       </div>
       <p
@@ -106,33 +101,6 @@ const index = () => {
       >
        As you browse through products, simply add them to your cart with a click. Shop Scan automatically calculates the total price of your selections in each selected store. This means you can see the total cost of your shopping list across all your chosen stores before making a purchase.
       </p>
-      {/* <div
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-      >
-        <Image src={savings} width={40} height={40} alt="chart" />
-        <h1
-          style={{
-            fontSize: "22px",
-            fontWeight: "600",
-            paddingBottom: "0px",
-            marginBottom: "0px",
-          }}
-          className={noir.className}
-        >
-          Find Where It's Cheaper to Buy Today
-        </h1>
-      </div>
-      <p
-        style={{ marginTop: "2px", fontSize: "17px" }}
-        className={noir.className}
-      >
-     Take the guesswork out of your
-        shopping decisions. Shoppy Scan lets you discover the most
-        cost-effective options by comparing prices at different stores. Whether
-        it's your regular grocery run or a special purchase, make sure you're
-        getting the best value for your money. Save time, save money, and shop
-        smarter with Shoppy Scan!
-      </p> */}
     </div>
   );
 };
