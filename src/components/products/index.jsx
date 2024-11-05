f6401e7ebe9e49b9467338344c766917fc9af12c
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2024-11-05 13:13:04 -0500

    Update index.jsx

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -538,7 +538,7 @@ const Products = ({ cartData }) => {
           Select the stores you'd like to compare prices for various products
         </p>
 
-                <div className="select-container">
+                  <div className="select-container">
           <div className="select-store">
             <label
               style={{
@@ -649,6 +649,72 @@ const Products = ({ cartData }) => {
               </div>
             )}
           </div>
+
+          {selectedAll.length > 0 && (
+            <div className="search" onKeyDown={handleKeyDown} tabIndex="0">
+              <label
+                style={{ paddingRight: "8px", fontSize: "18px" }}
+                className={`${noir.className} label`}
+              >
+                Search:
+              </label>
+              <input
+                className={noir.className}
+                placeholder="Search for..."
+                // style={{
+                //   padding: "0.375rem 2.25rem 0.375rem 0.75rem",
+                //   fontSize: "1rem",
+                //   marginRight: "16px",
+                //   fontWeight: "400",
+                //   lineHeight: "1.5",
+                //   color: "#212529",
+                //   backgroundColor: "#fff",
+                //   border: "1px solid #ced4da",
+                //   borderRadius: "0.25rem",
+                //   transition:
+                //     "border-color .15s ease-in-out,box-shadow .15s ease-in-out",
+                //   width: "120px",
+                // }}
+                type="text"
+                value={searchText}
+                onChange={handleSearchChange}
+                required
+              />
+
+              <button
+                className={noir.className}
+                style={{
+                  outline: "0",
+                  height: "38px",
+                  cursor: "pointer",
+                  padding: "5px 16px",
+                  fontSize: "14px",
+                  fontWeight: "500",
+                  lineHeight: "20px",
+                  verticalAlign: "middle",
+                  border: "1px solid",
+                  borderRadius: " 6px",
+                  color: " #24292e",
+                  backgroundColor: "#fafbfc",
+                  borderColor: "#1b1f2326",
+                  boxShadow:
+                    "rgba(27, 31, 35, 0.04) 0px 1px 0px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset",
+                  transition: "0.2s cubic-bezier(0.3, 0, 0.5, 1)",
+                }}
+                //disabled={searchText === null || selectedLocation === null}
+                onClick={handleButtonClick}
+                ref={buttonRef}
+                //  disabled={!searchText || !selectedLocation || selectedAllLength && selectedAllLength.length === 0}
+                disabled={
+                  !searchText ||
+                  (selectedAllLength && selectedAllLength.length === 0)
+                }
+              >
+                Search
+              </button>
+            </div>
+          )}
+        </div>
 
           {selectedAll.length > 0 && (
             <div className="search" onKeyDown={handleKeyDown} tabIndex="0">
