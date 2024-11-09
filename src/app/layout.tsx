320dda5c5ca9d405ee06b99906fc6a1200edbeb6
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2024-11-09 18:16:06 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,6 @@ export default function RootLayout({
             {children}
            <SpeedInsights />
             <Analytics />
-            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1262441687811052"
-     crossOrigin="anonymous"></Script>
       </body>
     </html>
   )
