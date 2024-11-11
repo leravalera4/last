14cd1c0251db1ea05c14467a9ae8b1a79164e2a4
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2024-11-11 12:21:14 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,44 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
 import Header from 'src/components/header'
 import Script from 'next/script'
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
-
-
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Shoppy Scan',
   description: 'Spend Wisely',
+  keywords: 'spend,compare,save,money,products,grocery,nofrills,fortinos,compare prices,best deals,shopping deals,compare prices between stores,great deals,shopping price comparison,site to compare prices of products,sale price finder',
+  openGraph: {
+    title: 'Shoppy Scan',
+    description: 'Spend wisely',
+    url: 'https://shoppyscan.ca',
+  },
+  robots: 'index, follow',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-<head>
- <title>Shoppy Scan</title>
- <meta name="google-site-verification" content="j0SKXll7FJFhpuNBjySgfPXEnc-UOLgsQAnoeGTn6l0" />
-          <meta name="description" content="Spend wisely" />
+      <head>
+        <meta name="google-site-verification" content="j0SKXll7FJFhpuNBjySgfPXEnc-UOLgsQAnoeGTn6l0" />
+        <meta name="description" content="Spend wisely" />
         <meta name="keywords" content="spend,compare,save,money,products,grocery,nofrills,fortinos,compare prices,best deals,shopping deals,compare prices between stores,great deals,shopping price comparison,site to compare prices of products,sale price finder" />
         <meta property="og:title" content="Shoppy Scan" />
         <meta property="og:description" content="Spend wisely" />
         <meta property="og:url" content="https://shoppyscan.ca" />
-        <meta name="robots" content="index, follow, save, compare" />
-<link rel="icon" href="/favicon.ico">
-  <Script async 
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1262441687811052"
-          crossOrigin="anonymous"/>
-</head>
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" href="/favicon.ico" />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1262441687811052" crossOrigin="anonymous" />
+      </head>
       <body className={inter.className}>
-            <Header />
-            {children}
-           <SpeedInsights />
-            <Analytics />
+        <Header />
+        {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
