e22e05efc09f9953b1a8f225fd6d9c3e8cbb5f8f
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2025-01-06 17:45:27 -0500

    Update index.jsx

--- src/components/sale/index.jsx
+++ src/components/sale/index.jsx
@@ -820,8 +820,8 @@ const Index = () => {
             >
               Select Location:
             </label>
-{/*                         <select
-              className={noir.className}
+                        <select
+               className={`${noir.className} select`}
               onChange={(e) => setSelectedLocation(e.target.value)}
               value={selectedLocation}
               onKeyDown={(e) => {
@@ -842,8 +842,8 @@ const Index = () => {
                   {location}
                 </option>
               ))}
-            </select> */}
-            <select
+            </select>
+{/*             <select
               className={`${noir.className} select`}
               // style={{
               //   height: "38px",
@@ -895,7 +895,7 @@ const Index = () => {
                   {location}
                 </option>
               ))}
-            </select>
+            </select> */}
           </>
         )}
 
