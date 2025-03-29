18688d3c5f3d95b4c1dbf2e4fd50b634ef2d4c87
leravalera4 <lerik2008_97@mail.ru>
2025-03-29 14:08:31 -0400

    Mobile

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -106,6 +106,7 @@ const Header = () => {
             display: "flex",
             alignItems: "center",
             justifyContent: "space-between",
+            backgroundColor: "white"
           }}
         >
           <div
