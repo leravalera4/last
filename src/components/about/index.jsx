c365b330c6abc6b065dbc02af34429123102aa0a
leravalera4 <lerik2008_97@mail.ru>
2025-04-01 11:51:01 -0400

    Page structure update

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -65,23 +65,7 @@ const Index = () => {
     <>
       {isMobile ? (
         <div className="h1">
-          <p
-            className={`${noir.className} p1`}
-            style={{
-              marginTop: "3%",
-              marginBottom: "3rem",
-              fontSize: "1.25rem",
-              fontWeight: 700,
-              textAlign: "center",
-            }}
-          >
-            {/*         Welcome to Shoppy Scan, your ultimate companion for savvy shopping and significant savings! */}
-            Welcome to Shoppy Scan, your reliable guide for smarter shopping and
-            significant savings.
-            <br />
-            Our goal is to empower you with tools and information that simplify
-            the shopping process while maximizing your budget.
-          </p>
+          {/* Section 1: Compare Prices */}
           <section style={{ paddingBottom: "1.5rem", paddingTop: "1.5rem" }}>
             <div
               style={{
@@ -98,7 +82,7 @@ const Index = () => {
                   fontWeight: 700,
                 }}
               >
-                Compare Prices Across Stores
+                Select Multiple Stores
               </h4>
 
               <div
@@ -118,10 +102,10 @@ const Index = () => {
                   style={{ paddingTop: "18px" }}
                 />
                 <p style={{ textAlign: "center" }} className={noir.className}>
-                  Easily compare prices for your favorite products across
-                  multiple stores. Our platform helps you find the best deals at
-                  a glance. Make smarter purchasing decisions and save money
-                  with side-by-side price comparisons.
+                  With Shoppy Scan, you have the power to choose from a vast
+                  array of stores. Whether it's your local supermarket, favorite
+                  online retailer, or specialty boutique, you can effortlessly
+                  add them to your list of selected stores.
                 </p>
               </div>
             </div>
@@ -143,7 +127,7 @@ const Index = () => {
                   fontWeight: 700,
                 }}
               >
-                Stay Updated on Discounts and Promotions
+                Compare Prices
               </h4>
 
               <div
@@ -163,11 +147,10 @@ const Index = () => {
                   style={{ paddingTop: "18px" }}
                 />
                 <p style={{ textAlign: "center" }} className={noir.className}>
-                  Never miss an opportunity to save with Shoppy Scan. Stay
-                  updated with the latest discounts, exclusive offers, and
-                  seasonal promotions. From flash sales to limited-time deals,
-                  we keep you informed about the best opportunities to save on
-                  the products you love.
+                  Shoppy Scan empowers you to compare prices across all the
+                  stores you've selected. Easily find the best deals and make
+                  informed decisions on your purchases. Say goodbye to guesswork
+                  and hello to savings!
                 </p>
               </div>
             </div>
@@ -189,7 +172,7 @@ const Index = () => {
                   fontWeight: 700,
                 }}
               >
-                Organize and Track Your Shopping
+                Add to List for Total Price
               </h4>
 
               <div
@@ -209,58 +192,11 @@ const Index = () => {
                   style={{ paddingTop: "18px" }}
                 />
                 <p style={{ textAlign: "center" }} className={noir.className}>
-                  Shoppy Scan makes managing your shopping list effortless. Add
-                  items to your virtual cart to keep track of products you’re
-                  interested in, get real-time price updates, compare options,
-                  and choose the best time to buy. It’s the perfect tool for
-                  smart shoppers who want to optimize their spending without the
-                  hassle of manual tracking.
-                </p>
-              </div>
-            </div>
-          </section>
-
-          <section style={{ paddingBottom: "1.5rem", paddingTop: "1.5rem" }}>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <h4
-                className={noir.className}
-                style={{
-                  margin: "0 0 -0.5rem 0px",
-                  fontSize: "1.3rem",
-                  fontWeight: 700,
-                }}
-              >
-                Shop Smarter, Anytime, Anywhere
-              </h4>
-
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  flexDirection: "column",
-                }}
-              >
-                <Image
-                  src={forth}
-                  width={150}
-                  height={150}
-                  alt="Chart Icon"
-                  priority
-                  key={forth}
-                  style={{ paddingTop: "18px" }}
-                />
-                <p style={{ textAlign: "center" }} className={noir.className}>
-                  Never miss a chance to save with Shoppy Scan. Accessible on
-                  all your devices, our platform is your convenient shopping
-                  companion. Whether you're at home or on the go, enjoy a
-                  seamless, user-friendly experience. Shop with confidence and
-                  make smarter purchasing decisions every time.
+                  As you browse through products, simply add them to your cart
+                  with a click. Shop Scan automatically calculates the total
+                  price of your selections in each selected store. This means
+                  you can see the total cost of your shopping list across all
+                  your chosen stores before making a purchase.
                 </p>
               </div>
             </div>
