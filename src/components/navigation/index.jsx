e6ceecbf4c1d9fc1805113e784a10881f8b9dfb6
leravalera4 <lerik2008_97@mail.ru>
2025-04-07 10:56:54 -0400

    Buttons

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,61 +1,3 @@
-// "use client";
-
-// import React from "react";
-// import Link from "next/link";
-// import localFont from "next/font/local";
-// import "./style.css";
-// import { usePathname } from "next/navigation";
-
-// const noir = localFont({
-//   src: [
-//     {
-//       path: "../../app/fonts/NoirPro-Light.ttf",
-//       weight: "200",
-//       style: "normal",
-//     },
-//     {
-//       path: "../../app/fonts/NoirPro-Regular.ttf",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     {
-//       path: "../../app/fonts/NoirPro-Bold.ttf",
-//       weight: "700",
-//       style: "normal",
-//     },
-//   ],
-// });
-
-// const index = () => {
-//   const pathname = usePathname();
-
-//   // Function to determine if a route is active
-
-//   return (
-//     <div>
-//     <nav style={{display: "flex",
-//       alignItems: "center",
-//       justifyContent: "center"}}>
-//         <Link style={{paddingRight:'16px'}}
-//           href="/"
-//           className={`${noir.className} ${pathname === '/' ? 'active' : 'link'}`} 
-//         >
-//           Special Price
-//         </Link>
-//         <Link
-//           href="/about"
-//           className={`${noir.className} ${pathname === '/about' ? 'active' : 'link'}`} 
-//         >
-//           Compare Prices
-//         </Link>
-//     </nav>
-//     </div>
-//   );
-// };
-
-// export default index;
-
-
 "use client";
 
 import React, { useState } from "react";
@@ -101,7 +43,7 @@ const HamburgerMenu = () => {
       </div>
       <div className={`overlay ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(false)}></div>
       
-      <nav className={`menu ${isOpen ? "open" : ""}`}>
+      <nav style={{gap:'20px'}} className={`menu ${isOpen ? "open" : ""}`}>
         <Link
           href="/"
           className={`${noir.className} ${pathname === '/' ? 'active' : 'link'}`}
