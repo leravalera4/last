ffe0374e581d0743fcf838e3221dc7a8fc646246
leravalera4 <lerik2008_97@mail.ru>
2025-04-10 16:23:59 -0400

    Initial commit

--- src/components/social/index.jsx
+++ src/components/social/index.jsx
@@ -14,59 +14,51 @@ import facebook from "../../app/images/facebook.svg";
 import x from "../../app/images/x.svg";
 import Image from "next/image.js";
 
-const socialLinks = [
-  { href: "https://www.instagram.com/shoppyscan.ca/", icon: <FaInstagram /> },
-  { href: "https://facebook.com/shoppyscan", icon: <FaFacebook /> },
-  { href: "https://twitter.com/shoppyscan", icon: <FaXTwitter /> },
-  { href: "https://linkedin.com/company/shoppyscan", icon: <FaLinkedin /> },
-  { href: "https://www.reddit.com/r/ShoppyScan/", icon: <FaReddit /> },
-  { href: "https://medium.com/@shoppyscan", icon: <FaMedium /> },
-];
-
 const SocialIcons = () => {
   return (
-    <div style={{ display: "flex", gap: "1rem" }}>
-      <Image
-        className="image"
-        alt="added"
-        src={instagram}
-        width={70}
-        height={70}
-        style={{ fill: "black" }}
-      >
-        <a href="https://www.instagram.com/shoppyscan.ca/" />
-      </Image>
-      <Image
-        className="image"
-        alt="added"
-        src={facebook}
-        width={70}
-        height={70}
-        style={{ fill: "black" }}
-      >
-        <a href="https://www.instagram.com/shoppyscan.ca/" />
-      </Image>
-      <Image
-        className="image"
-        alt="added"
-        src={linkedin}
-        width={70}
-        height={70}
-        style={{ fill: "black" }}
-      >
-        <a href="https://www.linkedin.com/company/shoppyscan" />
-      </Image>
-      <Image
-        className="image"
-        alt="added"
-        src={x}
-        width={60}
-        height={60}
-        style={{ fill: "black" }}
-      >
-        <a href="https://x.com/shoppyscan" />
-      </Image>
-    </div>
+<div style={{ display: "flex", gap: "1rem" }}>
+  <a href="https://www.instagram.com/shoppyscan.ca/" target="_blank" rel="noopener noreferrer">
+    <Image
+      className="image"
+      alt="Instagram"
+      src={instagram}
+      width={70}
+      height={70}
+      style={{ filter: "brightness(0)" }}
+    />
+  </a>
+  <a href="https://www.facebook.com/shoppyscan" target="_blank" rel="noopener noreferrer">
+    <Image
+      className="image"
+      alt="Facebook"
+      src={facebook}
+      width={70}
+      height={70}
+      style={{ filter: "brightness(0)" }}
+    />
+  </a>
+  <a href="https://www.linkedin.com/company/shoppyscan" target="_blank" rel="noopener noreferrer">
+    <Image
+      className="image"
+      alt="LinkedIn"
+      src={linkedin}
+      width={70}
+      height={70}
+      style={{ filter: "brightness(0)" }}
+    />
+  </a>
+  <a href="https://x.com/shoppyscan" target="_blank" rel="noopener noreferrer">
+    <Image
+      className="image"
+      alt="X"
+      src={x}
+      width={60}
+      height={60}
+      style={{ filter: "brightness(0)" }}
+    />
+  </a>
+</div>
+
   );
 };
 
