8c03cd96fb874291a57f2cc14a2c75443826455b
leravalera4 <lerik2008_97@mail.ru>
2025-04-14 19:05:54 -0400

    Cart width

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1164,7 +1164,7 @@ const Products = ({ cartData }) => {
                 }}
                // value={selectedStore}
                 style={{
-                  // marginRight: "20px",
+                  marginRight: "0px",
                   width: isMobile ? "100%" : "200px",
                   marginBottom: isMobile && "10px",
                   fontSize: isMobile && "16px",
