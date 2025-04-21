3b806ec121392c006b7f7437391cd54745bf42a9
leravalera4 <lerik2008_97@mail.ru>
2025-04-21 16:51:00 -0400

    Mobile version Header etc

--- src/components/sale/index.jsx
+++ src/components/sale/index.jsx
@@ -2048,6 +2048,7 @@ const Index = () => {
                                         justifyContent: "center",
                                         width: "80%",
                                         height: "39px",
+                                        cursor: "none"
                                       }}
                                     >
                                       <button
