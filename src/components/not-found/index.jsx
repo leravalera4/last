f66020bc52ad5349a7cc398647899c3a17939541
leravalera4 <lerik2008_97@mail.ru>
2025-04-28 23:41:34 -0400

    feat: add onboarding for compare prices and stores near me

--- src/components/not-found/index.jsx
+++ src/components/not-found/index.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import notFound from "../../app/images/notFound.svg";
+import React from "react";
+import notFound from "../../app/images/not_f.svg";
+import Image from "next/image.js";
+import localFont from "next/font/local";
+
+const noir = localFont({
+  src: [
+    {
+      path: "../../app/fonts/NoirPro-Light.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../app/fonts/NoirPro-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../app/fonts/NoirPro-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 const index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <div style={{ display: "flex", flexDirection: "row", alignItems: "center",justifyContent:'center'}}>
+      <Image src={notFound} alt="Not Found" width={200} height={200}/>
+      <h1 className = {noir.className} style={{ textAlign: "center", marginTop: "20px" }}>
+        Oops! No results found.
+      </h1>
+    </div>
+  );
+};
 
-export default index
+export default index;
