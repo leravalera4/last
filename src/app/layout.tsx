2a7da16e5f0f617a498b4c80d2b3bc23e80aef80
leravalera4 <lerik2008_97@mail.ru>
2025-07-11 16:16:13 -0400

    feat: add onboarding for compare

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,6 +83,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta
